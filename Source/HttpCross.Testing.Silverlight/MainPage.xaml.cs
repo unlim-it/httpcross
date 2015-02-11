080bd3bf6dfc09cbfb4f6e7411825757a1330aa0
Denis Kurilenko <kurilenkodenis@gmail.com>
2015-02-11 13:42:46 +0200

    added silverlight test project

---
+++ Source/HttpCross.Testing.Silverlight/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿namespace HttpCross.Testing.Silverlight
+{
+    using System;
+    using System.Windows;
+
+    public partial class MainPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            this.Loaded += this.OnLoaded;
+        }
+
+        private void OnLoaded(object sender, RoutedEventArgs e)
+        {
+            Http.Post("http://localhost:38822/")
+                .WithBody(new { hey = "zmey" })
+                .ExecuteResult<Object>()
+                .ContinueWith(
+                    task =>
+                    {
+                        var result = task.Result;
+                        
+                    });
+        }
+    }
+}
--- Source/HttpCross/HttpRequestBuilder.cs
+++ Source/HttpCross/HttpRequestBuilder.cs
@@ -1,15 +1,22 @@
 ﻿namespace HttpCross
 {
+    using System.IO;
     using System.Net;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    using Newtonsoft.Json;
 
     public class HttpRequestBuilder
     {
-        private readonly WebRequest request;
+        private readonly HttpWebRequest request;
+        private byte[] data;
 
         internal HttpRequestBuilder(string url, string method)
         {
-            this.request = WebRequest.Create(url);
-            request.Method = method;
+            this.request = (HttpWebRequest)WebRequest.Create(url);
+            this.request.Method = method;
+            this.data = null;
         }
 
         public HttpRequestBuilder WithHeader(string name, string value)
@@ -20,7 +27,60 @@
 
         public HttpRequestBuilder WithBody(object body)
         {
-            return this;
+            this.data = Encoding.UTF8.GetBytes(body.ToString());
+            return this.WithHeader("ContentLength", data.Length.ToString());
+        }
+
+        public Task<TResult> ExecuteResult<TResult>()
+        {
+            var resultTask = this.Execute()
+                .ContinueWith(task =>
+                {
+                    var responseStream = task.Result.GetResponseStream();
+
+                    using (responseStream)
+                    using (var streamReader = new StreamReader(responseStream))
+                    using (var jsonReader = new JsonTextReader(streamReader))
+                    {
+                        var serializer = new JsonSerializer();
+                        var result = serializer.Deserialize<TResult>(jsonReader);
+                        return result;
+                    }
+                });
+
+            return resultTask;
+        }
+
+        public Task<WebResponse> Execute()
+        {
+            Task<WebResponse> responseTask;
+
+            if (this.data != null)
+            {
+                responseTask = Task.Factory
+                    .FromAsync<Stream>(this.request.BeginGetRequestStream, this.request.EndGetRequestStream, this.request)
+                    .ContinueWith(
+                        task =>
+                        {
+                            var requestStream = task.Result;
+                            using (requestStream)
+                            {
+                                requestStream.Write(this.data, 0, this.data.Length);
+                            }
+
+                            var webResponse = Task<WebResponse>.Factory
+                                .FromAsync(this.request.BeginGetResponse, this.request.EndGetResponse, this.request).Result;
+
+                            return webResponse;
+                        });
+
+                return responseTask;
+            }
+
+            responseTask = Task<WebResponse>.Factory
+                .FromAsync(this.request.BeginGetResponse, this.request.EndGetResponse, this.request);
+
+            return responseTask;
         }
     }
 }
