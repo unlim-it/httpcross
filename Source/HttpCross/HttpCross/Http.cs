abc2afb1a5641ac26fda69837f6cbda6221bfebd
Denis Kurilenko <kurilenkodenis@gmail.com>
2015-02-11 03:19:18 +0200

    create solution and add http and builder classes.

---
+++ Source/HttpCross/HttpCross/Http.cs
@@ -0,0 +1,37 @@
+﻿namespace HttpCross
+{
+    public class Http
+    {
+        /// <summary>
+        /// Requests a representation of the specified resource.
+        /// </summary>
+        public static HttpRequestBuilder Get(string url)
+        {
+            return new HttpRequestBuilder(url, "GET");
+        }
+
+        /// <summary>
+        /// Submits data to be processed to a specified resource.
+        /// </summary>
+        public static HttpRequestBuilder Post(string url)
+        {
+            return new HttpRequestBuilder(url, "POST");
+        }
+
+        /// <summary>
+        /// Requests that the enclosed entity be stored under the supplied URI.
+        /// </summary>
+        public static HttpRequestBuilder Put(string url)
+        {
+            return new HttpRequestBuilder(url, "PUT");
+        }
+
+        /// <summary>
+        /// Deletes the specified resource.
+        /// </summary>
+        public static HttpRequestBuilder Delete(string url)
+        {
+            return new HttpRequestBuilder(url, "DELETE");
+        }
+    }
+}
---
+++ Source/HttpCross/HttpCross/HttpRequestBuilder.cs
@@ -0,0 +1,15 @@
+﻿namespace HttpCross
+{
+    using System.Net;
+
+    public class HttpRequestBuilder
+    {
+        private readonly WebRequest request;
+
+        internal HttpRequestBuilder(string url, string method)
+        {
+            this.request = WebRequest.Create(url);
+            request.Method = method;
+        }
+    }
+}
