dddb1820991cc60cb41aa3deaf6dd82ef68c87d1
Denis Kurilenko <kurilenkodenis@gmail.com>
2015-02-11 13:43:01 +0200

    nissing files

---
+++ Source/HttpCross.Testing.Desktop/HttpCrossTests.cs
@@ -0,0 +1,15 @@
+﻿namespace HttpCross.Testing.Desktop
+{
+    using System.IO;
+    using NUnit.Framework;
+
+    public class HttpCrossTests
+    {
+        [Test]
+        public async void Test1()
+        {
+            var result = await Http.Get("http://google.com")
+                .ExecuteResult<string>();
+        }
+    }
+}
