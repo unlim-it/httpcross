9e65b627dfc470ecfa76e5c0a65c2f4473b3fdc1
Denis Kurilenko <kurilenkodenis@gmail.com>
2015-02-24 23:54:11 +0200

    add startup for OWIN Api

---
+++ Source/HttpCross.Testing.API/Startup.cs
@@ -0,0 +1,20 @@
+﻿﻿[assembly: Microsoft.Owin.OwinStartup(typeof(HttpCross.Testing.API.Startup))]
+namespace HttpCross.Testing.API
+{
+    using System.Web.Http;
+    using Owin;
+
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var config = new HttpConfiguration();
+
+            config.MapHttpAttributeRoutes();
+            config.Routes.MapHttpRoute("Default", "api/{controller}/{id}", new { id = RouteParameter.Optional });
+
+
+            app.UseWebApi(config);
+        }
+    }
+}
